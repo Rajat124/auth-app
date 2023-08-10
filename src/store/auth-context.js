ff7aae417a113f67b512023a022c9176704dca45
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-08-10 10:58:18 +0530

    Alternative to persist the login state.

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,12 +7,8 @@ export const AuthContext = () => {
 };
 
 const Context = (props) => {
-  const [token, setToken] = useState(null);
-
-  useEffect(() => {
-    const tok = localStorage.getItem("token");
-    setToken(tok);
-  }, []);
+  const intialtoken = localStorage.getItem("token");
+  const [token, setToken] = useState(intialtoken);
 
   const usersIsLoggedIn = !!token;
 
